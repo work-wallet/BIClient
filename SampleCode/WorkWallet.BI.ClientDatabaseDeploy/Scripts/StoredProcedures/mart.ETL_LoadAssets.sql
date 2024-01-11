ca7240cfcc7a407cf401e1769a54528dbc6ddb26
Adrian Howlett <adrian-code@owlit.co.uk>
2024-01-11 17:08:04 +0000

    Implemented MaintainAssetDimension

---
+++ SampleCode/WorkWallet.BI.ClientDatabaseDeploy/Scripts/StoredProcedures/mart.ETL_LoadAssets.sql
@@ -0,0 +1,137 @@
+DROP PROCEDURE IF EXISTS mart.ETL_LoadAssets;
+GO
+
+CREATE PROCEDURE mart.ETL_LoadAssets
+    @json nvarchar(max)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION
+
+        -- maintain the Wallet dimension table
+
+        DECLARE @walletTable mart.ETL_WalletTable;
+
+        INSERT INTO @walletTable
+        (
+            WalletId
+            ,Wallet
+        )
+        SELECT * FROM OPENJSON(@json, '$.Wallets')
+        WITH
+        (
+            WalletId uniqueidentifier
+            ,Wallet nvarchar(50)
+        );
+
+        EXEC mart.ETL_MaintainWalletDimension @walletTable = @walletTable;
+
+        -- maintain the Asset table
+
+        DECLARE @assetTable mart.ETL_AssetTable;
+
+        INSERT INTO @assetTable
+        (
+			AssetId
+			,AssetType
+			,AssetStatusCode
+			,Reference
+			,[Name]
+			,[Description]
+			,CreatedOn
+			,WalletId
+        )
+        SELECT * FROM OPENJSON(@json, '$.Assets')
+        WITH
+        (
+			AssetId uniqueidentifier
+			,AssetType nvarchar(75)
+			,AssetStatusCode int
+			,Reference nvarchar(143)
+			,[Name] nvarchar(75)
+			,[Description] nvarchar(max)
+			,CreatedOn datetimeoffset(7)
+			,WalletId uniqueidentifier
+        );
+
+        EXEC mart.ETL_MaintainAssetDimension @assetTable = @assetTable;
+
+        --EXEC mart.ETL_DeleteAssetFacts @assetTable = @assetTable;
+
+        -- load the AssetProperty data
+
+        DECLARE @assetPropertyTable mart.ETL_AssetPropertyTable;
+
+        INSERT INTO @assetPropertyTable
+        (
+			AssetId
+			,Property
+			,PropertyType
+			,[Value]
+			,WalletId
+        )
+        SELECT * FROM OPENJSON(@json, '$.Properties')
+        WITH
+        (
+			AssetId uniqueidentifier
+			,Property nvarchar(250)
+			,PropertyType nvarchar(20)
+			,[Value] nvarchar(max)
+			,WalletId uniqueidentifier
+        );
+
+        -- EXEC mart.ETL_LoadAssetProperties @assetPropertyTable = @assetPropertyTable
+
+        -- load the AssetAssignment data
+
+        DECLARE @assetAssignmentTable mart.ETL_AssetAssignmentTable;
+
+        INSERT INTO @assetAssignmentTable
+        (
+			AssetId
+			,AssignedOn
+			,AssignmentType
+			,AssignedTo
+			,CompanyId
+			,Company
+			,SiteId
+			,[Site]
+			,WalletId
+        )
+        SELECT * FROM OPENJSON(@json, '$.Assignments')
+        WITH
+        (
+			AssetId uniqueidentifier
+			,AssignedOn datetimeoffset(7)
+			,AssignmentType nvarchar(20)
+			,AssignedTo nvarchar(max)
+			,CompanyId uniqueidentifier
+			,Company nvarchar(max)
+			,SiteId uniqueidentifier
+			,[Site] nvarchar(max)
+			,WalletId uniqueidentifier
+        );
+
+        -- EXEC mart.ETL_LoadAssetAssignments @assetAssignmentTable = @assetAssignmentTable
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN;
+
+        DECLARE @ErrorMessage NVARCHAR(4000);  
+        DECLARE @ErrorSeverity INT;  
+        DECLARE @ErrorState INT;  
+
+        SELECT   
+           @ErrorMessage = ERROR_MESSAGE(),  
+           @ErrorSeverity = ERROR_SEVERITY(),  
+           @ErrorState = ERROR_STATE();  
+
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+    END CATCH
+    END
+GO
---
+++ SampleCode/WorkWallet.BI.ClientDatabaseDeploy/Scripts/StoredProcedures/mart.ETL_MaintainAssetDimension.sql
@@ -0,0 +1,74 @@
+DROP PROCEDURE IF EXISTS mart.ETL_MaintainAssetDimension;
+GO
+
+CREATE PROCEDURE mart.ETL_MaintainAssetDimension @assetTable mart.ETL_AssetTable READONLY
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	WITH cte
+	AS
+	(
+		SELECT
+			a.AssetId
+			,a.AssetType
+			,astat.AssetStatus_key
+			,a.Reference
+			,a.[Name]
+			,a.[Description]
+			,a.CreatedOn
+			,w.Wallet_key
+		FROM @assetTable AS a
+		INNER JOIN mart.AssetStatus AS astat ON a.AssetStatusCode = astat.AssetStatusCode
+		INNER JOIN mart.Wallet AS w ON a.WalletId = w.WalletId
+	)
+	MERGE mart.[Asset] AS tgt
+	USING
+		cte AS src ON tgt.AssetId = src.AssetId
+	WHEN MATCHED AND
+	(
+		tgt.AssetType <> src.AssetType
+		OR tgt.Reference <> src.Reference
+		OR tgt.[Name] <> src.[Name]
+		OR tgt.[Description] <> src.[Description]
+		OR tgt.CreatedOn <> src.CreatedOn
+		OR tgt.Wallet_key <> src.Wallet_key
+	)
+	THEN UPDATE
+		SET
+			AssetType = src.AssetType
+			,Reference = src.Reference
+			,[Name] = src.[Name]
+			,[Description] = src.[Description]
+			,CreatedOn = src.CreatedOn
+			,Wallet_key = src.Wallet_key
+			,_edited = SYSUTCDATETIME()
+	WHEN NOT MATCHED
+	THEN
+		INSERT
+		(
+			AssetId
+			,AssetType
+			,AssetStatus_key
+			,Reference
+			,[Name]
+			,[Description]
+			,CreatedOn
+			,Wallet_key
+		)
+		VALUES
+		(
+			src.AssetId
+			,src.AssetType
+			,src.AssetStatus_key
+			,src.Reference
+			,src.[Name]
+			,src.[Description]
+			,src.CreatedOn
+			,src.Wallet_key
+		);
+
+    PRINT 'MERGE mart.Asset, number of rows = ' + CAST(@@ROWCOUNT AS varchar);
+
+END
+GO
